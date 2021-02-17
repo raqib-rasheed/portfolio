0e9e44174393420d79f76dc6aa078560e2e0c890
raqib-rasheed <raqib.abdulrasheed@outlook.com>
2021-02-17 16:17:56 +0530

    home page text edit

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ const HomePage = () => {
             </a>
           ))}
         </div>
-        <h4> Front - end developer </h4>
+        <h4> Frontend developer </h4>
       </HomePageStyled>
     </div>
   );
